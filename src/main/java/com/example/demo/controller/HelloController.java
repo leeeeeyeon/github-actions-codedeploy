16baadfd19e3ddcae37e627a16b364b0f92da1b9
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-02-02 17:38:34 +0900

    feat: Hello API 수정

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -16,7 +16,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "hello codedeploy";
+        return "hello world";
     }
 
     @GetMapping("/hello/test")
