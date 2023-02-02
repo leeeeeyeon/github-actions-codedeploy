c88d08e62f088628239325069e7c584684f9dc1b
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-02-02 18:41:49 +0900

    refactor: 안 쓰는 import 제거

--- src/main/java/com/example/demo/config/redis/RedisConfig.java
+++ src/main/java/com/example/demo/config/redis/RedisConfig.java
@@ -4,13 +4,9 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
